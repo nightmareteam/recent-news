bf6d6697ee947c3f9753b75aab01e8dbf00fc335
Jin <jinruncai@gmail.com>
2020-04-03 14:21:12 -0700

    Write schema and seeding script to generate data

---
+++ database/seedUpdates.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const fs = require('fs');
+const faker = require('faker');
+
+const outFile = fs.createWriteStream(
+    path.resolve(__dirname, 'data', 'updates.csv')
+)
+
+// Write fields on first line
+outFile.write('game_id, posted_by, post_date, title, text, img, comment_count\n', () => {
+    writeUpdates();
+});
+
+const writeUpdates = async () => {
+    for (let i = 0; i < 10000000; i++) {
+        const updates = Math.floor(Math.random() * 10);
+        for (let j = 0; j < updates; j++) {
+            const record = [
+                i, // game_id
+                faker.internet.userName(), // posted_by
+                faker.date.past(), // post_date
+                '"' + faker.lorem.sentence(4, false, 4) + '"', // title
+                faker.lorem.paragraph(3, false, 5), // text 
+                faker.image.abstract(), // img
+                faker.random.number(), // comment_count
+            ]
+            const csv = record.join(',') + '\n';
+            if (!outFile.write(csv)) {
+                await new Promise((resolve => outFile.once('drain', () => {
+                    resolve();
+                })));
+            };
+        }
+    }
+}
