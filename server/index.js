9bda1747829b452a2061f465469cb49679803bb3
Jin <jinruncai@gmail.com>
2020-04-08 16:14:25 -0700

    Add route for game updates

--- server/index.js
+++ server/index.js
@@ -1,40 +1,21 @@
 const express = require('express');
-const App = express();
 const bodyParser = require('body-parser');
+const app = express();
 const port = 3003;
 const db = require('../database');
-const path = require('path');
 
-App.use(bodyParser.json());
-App.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-App.use(express.static('public'));
+app.use(express.static('public'));
 
-App.listen(port, () => { console.log(`listening on port ${port}!`)});
-
-App.get('/Updates', (req, res) => {
-	db.getUpdates((err, data) => {
-		if (err) {
-			console.log('[server/index.jsx:18] err getting updates from db!', err);
-			return;
-		}
-
-		console.log('data received from db! chyaaa');
-		res.send(data);
-	})
+app.get('/:gameId/updates', (req, res) => {
+	const { gameId } = req.params;
+	db.getUpdatesByGameId(Number(gameId))
+		.then(({rows}) => {
+			res.send(rows);
+		})
+		.catch()
 })
 
-App.get('/Games/:id', (req, res) => {
-	console.log('get to server for games received!');
-	console.log('req params!', req.params);
-
-	db.getGame(req.params.id, (err, data) => {
-		if (err) {
-			console.log('[server/index.jsx: 33] err getting updates from db!', err);
-			return;
-		}
-
-		console.log('data received from db! chyaaa');
-		res.send(data);
-	})
-})
+app.listen(port, () => { console.log(`listening on port ${port}!`)});
