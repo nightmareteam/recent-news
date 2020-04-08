8faa420dc1b4fb63a71a9f1d6779b5a9cc22a211
Jin <jinruncai@gmail.com>
2020-04-08 16:13:28 -0700

    Add generic game updates query

--- database/index.js
+++ database/index.js
@@ -1,1 +1,19 @@
-const { Client } = require('pg');
+const { Pool } = require('pg');
+
+const pool = new Pool({
+	user: 'jin',
+	password: 'abc',
+	database: 'updates',
+	host: 'localhost',
+	port: 5432
+});
+
+const getUpdatesByGameId = (gameId) => {
+	return pool
+		.query('SELECT * FROM updates WHERE game_id = $1', [gameId])
+}
+
+module.exports = {
+	getUpdatesByGameId,
+	end: pool.end.bind(pool)
+}
