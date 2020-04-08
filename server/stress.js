9cfeff8ee6a3396a08d11b971d353c0df32a3e37
Jin <jinruncai@gmail.com>
2020-04-08 16:57:31 -0700

    Add transaction time check

--- server/stress.js
+++ server/stress.js
@@ -4,10 +4,10 @@ import { Rate } from 'k6/metrics';
 
 export let errorRate = new Rate('errors');
 
-
 export default function() {
     const gameId = Math.floor(Math.random() * 10000000);
     check(http.get(`http://localhost:3003/${gameId}/updates`), {
-        'status is 200': r => r.status == 200
+        'status is 200': r => r.status == 200,
+        'transaction time < 2000ms': r => r.timings.duration < 2000,
     }) || errorRate.add(1);
 }
