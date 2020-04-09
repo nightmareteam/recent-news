9fca031368d5d0cb9b26e5eacc6245e35e7a81ed
Jin <jinruncai@gmail.com>
2020-04-08 17:05:22 -0700

    Removeo unnecessary middleware

--- server/index.js
+++ server/index.js
@@ -4,11 +4,6 @@ const app = express();
 const port = 3003;
 const db = require('../database');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(express.static('public'));
-
 app.get('/:gameId/updates', (req, res) => {
 	const { gameId } = req.params;
 	db.getUpdatesByGameId(Number(gameId))
