f19a519c79feeb77a9b70642f65b9d691a6083a6
Jin Cai <jinruncai@gmail.com>
2020-05-03 16:11:06 -0700

    Allow cross origin requests in development

--- server/index.js
+++ server/index.js
@@ -4,6 +4,21 @@ const express = require('express');
 const db = require('../database');
 const app = express();
 
+const allowedOrigins = [
+	'localhost:3000', // Proxy
+	'localhost:9000', // webpack-dev-server
+	'localhost:5500', // VsCode live-server
+]
+
+app.use('/', function (req, res, next) {
+	const { origin } = req.headers;
+	if (allowedOrigins.includes(origin)) {
+		res.setHeader('Access-Control-Allow-Origin', origin);
+	}
+	next();
+});
+
+
 app.get('/recent-news/:gameId/updates', (req, res) => {
 	const { gameId } = req.params;
 	const { page } = req.query;
